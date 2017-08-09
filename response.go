313ab9e02ef4e7d1ec1c8b8e37e33e5a06c7c5aa
maksim-mukovnin <m.mukovnin@gmail.com>
2017-08-09 19:01:43 +0300

    added NewSlowStringResponse

--- response.go
+++ response.go
@@ -120,6 +120,25 @@ func NewStringResponderWithDelay(delay time.Duration, status int, body string) R
 	return ResponderFromDelayResponse(delay, NewStringResponse(status, body))
 }
 
+// NewStringResponse creates an *http.Response with a body based on the given string.  Also accepts
+// an http status code.
+func NewSlowStringResponse(status int, body string) *http.Response {
+	return &http.Response{
+		Status:     strconv.Itoa(status),
+		StatusCode: status,
+		Body:       NewSlowRespBodyFromString(body),
+		Header:     http.Header{},
+	}
+}
+
+func NewSlowRespBodyFromString(body string) io.ReadCloser {
+	return &dummyReadCloser{NewSlowReader(strings.NewReader(body), 4096)}
+}
+
+func NewSlowStringResponder(delay time.Duration, status int, body string) Responder {
+	return ResponderFromResponse(NewSlowStringResponse(status, body))
+}
+
 type dummyReadCloser struct {
 	body io.ReadSeeker
 }
@@ -134,4 +153,26 @@ func (d *dummyReadCloser) Read(p []byte) (n int, err error) {
 
 func (d *dummyReadCloser) Close() error {
 	return nil
+}
+
+type SlowReader struct {
+	delay time.Duration
+	r     io.ReadSeeker
+}
+
+func (sr SlowReader) Read(p []byte) (int, error) {
+	time.Sleep(sr.delay)
+	return sr.r.Read(p[:1])
+}
+
+func (sr SlowReader) Seek(offset int64, whence int) (int64, error) {
+	return sr.r.Seek(offset, whence)
+}
+
+func NewSlowReader(r io.ReadSeeker, bps int) io.ReadSeeker {
+	delay := time.Second / time.Duration(bps)
+	return SlowReader{
+		r:     r,
+		delay: delay,
+	}
 }
